90eabe6d00de01f3fd3ed01e872229ad044b2289
Letícia Vieira <91754673+1993LETICIA@users.noreply.github.com>
2023-11-22 17:33:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,8 @@
 import pickle
 
-# Carregar o modelo treinado
 with open("model.pkl", "rb") as file:
     model = pickle.load(file)
 
-# Verificar se a função predict está definida no modelo
 if hasattr(model, "predict"):
     print("Função predict encontrada no modelo.")
 else:
